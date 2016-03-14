b9e372f1b602cce3a8ba5d36db5de180a1cad55f
Nike <lysenkonike@gmail.com>
2016-03-14 11:30:31 +0300

    function_onload

--- js/global.js
+++ js/global.js
@@ -1,42 +1,29 @@
-var i = localStorage.number_theme ? localStorage.number_theme : 0;
-var theme = document.getElementsByTagName('link')[1];
-var arrayThemes = ['vendor/bootstrap-theme.css',
-    'vendor/bootstrap-theme1.css',
-    'vendor/bootstrap-theme2.css',
-    'vendor/bootstrap-theme3.css',
-    'vendor/bootstrap-theme4.css',
-    'vendor/bootstrap-theme5.css',
-    'vendor/bootstrap-theme6.css',
-    'vendor/bootstrap-theme7.css',
-    'vendor/bootstrap-theme8.css',
-    'vendor/bootstrap-theme9.css'
-];
+var elementTheme = document.getElementById('theme-link');
+var selectTheme = document.getElementsByName('selectTheme')[0];
 
 document.forms[0].number.oninput = function() {
     document.getElementById('translation_ru').textContent = translateNumber(this.value, 'ru');
     document.getElementById('translation_uk').textContent = translateNumber(this.value, 'uk');
     document.getElementById('translation_en_us').textContent = translateNumber(this.value, 'en-us');
     document.getElementById('translation_en').textContent = translateNumber(this.value, 'en');
-    localStorage.num = this.value;
-
-}
-
-window.onload = function() {
-    theme.href = arrayThemes[i];
-
-    if(localStorage.num){
-        document.forms[0].number.value = localStorage.num;
+    if (typeof Number(this.value) === 'number' &&  this.value != localStorage.number) {
+        localStorage.number = this.value;
     }
-
-    document.getElementById('translation_ru').textContent = translateNumber(localStorage.num, 'ru');
-    document.getElementById('translation_uk').textContent = translateNumber(localStorage.num, 'uk');
-    document.getElementById('translation_en_us').textContent = translateNumber(localStorage.num, 'en-us');
-    document.getElementById('translation_en').textContent = translateNumber(localStorage.num, 'en');
 }
 
-document.getElementById('next_theme').onclick = function() {
-    i++;
-    if(i == 10) i = 0;
-    theme.href = arrayThemes[i];
-    localStorage['number_theme'] = i;
+if (localStorage.number) {
+    document.forms[0].number.value = localStorage.number;
+    document.forms[0].number.oninput();
+}
+
+selectTheme.onchange = function() {
+    elementTheme.href = 'vendor/' + this.value + '-theme.css';
+    if (this.value != localStorage.actualTheme) {
+        localStorage.actualTheme = this.value;
+    }
+}
+
+if (localStorage.actualTheme) {
+    selectTheme.value = localStorage.actualTheme;
+    selectTheme.onchange();
 }
