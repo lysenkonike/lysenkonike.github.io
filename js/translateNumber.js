d808d70737a6e60b75000ec99528a90e1617fe70
Nike <lysenkonike@gmail.com>
2016-03-21 11:07:07 +0300

    if-space-does-not-translate

--- js/translateNumber.js
+++ js/translateNumber.js
@@ -1,5 +1,5 @@
 function translateNumber(number, lang) {
-    if (!isValid(number) || number.toString().length > 24 || /^0+/.test(number)) {
+    if (!isValid(number) || number.toString().length > 24 || /^0+/.test(number) || /^[' ']+/.test(number)) {
         return '';
     }
 
